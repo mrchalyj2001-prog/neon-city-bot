e06b12e23f3104b62116b23398a33c487e0afe22
mrchalyj2001-prog <mr.chalyj2001@gmail.com>
2026-05-21 17:30:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import Message
+from aiogram.filters import CommandStart
+import asyncio
+import os
 
+TOKEN = os.getenv("BOT_TOKEN")
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def start(message: Message):
+    await message.answer("Привет! RPG бот запущен.")
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
