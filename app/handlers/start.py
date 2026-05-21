e3e2d949a4ed47bdbbc319f6a600aa664f68dcbb
mrchalyj2001-prog <mr.chalyj2001@gmail.com>
2026-05-21 19:29:42 +0200

    Update start.py

--- app/handlers/start.py
+++ app/handlers/start.py
@@ -1,1 +1,9 @@
+from aiogram import Router
+from aiogram.types import Message
+from aiogram.filters import CommandStart
 
+router = Router()
+
+@router.message(CommandStart())
+async def start_handler(message: Message):
+    await message.answer("Добро пожаловать в NeonCity RPG 🌆")
